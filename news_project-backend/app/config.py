eb99ecc969119dcc19b0e2d1e6049ee35bd42517
Kuruguntla Asritha <asritha.kuruguntla@gmail.com>
2025-06-05 11:47:44 +0530

    Delete exposed API key

--- news_project-backend/app/config.py
+++
@@ -1,5 +0,0 @@
-NEWS_API_KEY = "aadbc39abdd84f55abf6ff640803549a"  # Replace with your key
-NEWS_API_URL = "https://newsapi.org/v2/top-headlines"
-CATEGORIES = ["business", "entertainment", "health", "science", "sports", "technology"]
-
-MODEL_NAME = "asritha22bce/bart-positive-tone-finetuned"
